7ad0ff339fa73ac60554bf17b2d91852c726b32a
Tandera, eye of <tandera@hackerspace.com>
2023-10-14 15:29:40 -0300

    Auth service added

---
+++ pocketpay/src/app/services/api/v1/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  private validateJWT(jwt: string): boolean {
+    const decodedJWT: any = jwtDecode(jwt);
+
+    console.log(decodedJWT);
+
+    if (decodedJWT.exp == null) return false;
+
+    const expirationDate = new Date(0);
+    expirationDate.setUTCSeconds(decodedJWT.exp);
+
+    if (expirationDate < new Date()) return false;
+
+    return true;
+  }
+
+  get jwt() {
+    const jwt = sessionStorage.getItem('session');
+
+    if (jwt != null && this.validateJWT(jwt)) {
+      return jwt;
+    } else {
+      throw new Error('Invalid JWT');
+    }
+  }
+
+
+  set jwt(jwt: string) {
+
+    if (!this.validateJWT(jwt)) {
+      throw new Error('Invalid JWT');
+    }
+
+    sessionStorage.setItem('session', jwt);
+  }
+}
