c4ff7433f77dbd7759e861a30ba501baf8ec7f7c
Vinicius Rebelatto <vinicius-rebelatto@hotmail.com>
2023-11-15 19:48:10 -0300

    dashboard seller

--- pocketpay/src/app/seller/seller-dashboard/sections/seller-dashboard-overview/seller-dashboard-overview.component.ts
+++ pocketpay/src/app/seller/seller-dashboard/sections/seller-dashboard-overview/seller-dashboard-overview.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/api/v1/auth/auth.service';
+import { TransactionService } from 'src/app/services/api/v1/transaction/transaction.service';
 
 @Component({
   selector: 'app-seller-dashboard-overview',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-dashboard-overview.component.scss']
 })
 export class SellerDashboardOverviewComponent {
+  public transactions$!: Observable<any>;
+  public displayedColumns: String[] = ['timestamp', 'type'];
 
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private transactionService: TransactionService,
+    private dialog: MatDialog
+  ) {
+    try {
+      const access_token = this.authService.jwt;
+      this.transactions$ = this.transactionService.getAllTransactions(access_token);
+    } catch (e) {
+      router.navigateByUrl('/client/auth/login');
+    }
+  }
 }
